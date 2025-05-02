baa32fe439a8f95592a98ee1d015b04b01668e9e
Alexander <ashabalin9999@gmail.com>
2025-05-02 10:08:45 +0200

    small fix

--- create_config.py
+++ create_config.py
@@ -97,6 +97,7 @@ def create_config(args):
     config.cluster_diffusion = args.cluster_diffusion
     config.random_init_embeddings = args.random_init_embeddings
     config.predict_tokens = args.predict_tokens
+    config.clamp = False
 
     decoder = config.decoder = create_decoder_config()
     decoder.dataset = data.datasets.datasets_list[0]
